d5bbb055d16abc8e178af4ccb2f7d84bc18218cd
SasunHakobyan <sasunhakobyan2002@gmail.com>
2022-11-12 07:13:32 -0800

    navbar routing

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import classes from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <div>
         <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' />
       </div>
