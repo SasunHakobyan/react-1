29eee13b6962d20c891fa421c1d98b456d9e767f
SasunHakobyan <sasunhakobyan2002@gmail.com>
2022-11-14 18:48:45 +0400

    React.createRef()

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,12 +2,15 @@ import DialogItem from './DialogItem/DialogItem';
 import Message from './Message/Message';
 
 import classes from './Dialogs.module.css';
+import React from 'react';
 
 const Dialogs = (props) => {
 
   let dialogsElements = props.state.dialogs.map(dialog => <DialogItem name={dialog.name} id={dialog.id} imgUrl={dialog.img_url} />);
 
   let messagesElements = props.state.messages.map(message => <Message isOwner={message.owner} message={message.message} />);
+
+  let messageData = React.createRef();
 
   return (
     <div className={classes.dialogs}>
@@ -16,6 +19,10 @@ const Dialogs = (props) => {
       </div>
       <div className={classes.messages}>
         {messagesElements}
+        <div className={classes.sendMessageContainer}>
+          <textarea ref={messageData}></textarea>
+          <button onClick={() => {alert(messageData.current.value)}}>Send</button>
+        </div>
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,18 +1,26 @@
+import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
   let postsElements = props.posts.map(post => <Post id={post.id} message={post.message} likeCount={post.likeCount} />);
 
+  let newPostElement = React.createRef();
+
+  function addPost() {
+    let text = newPostElement.current.value;
+    console.log(newPostElement);
+  }
+
   return (
     <div className={classes.postsBlock}>
       <h3>My posts</h3>
       <div>
         <div>
-          <textarea></textarea>
+          <textarea ref={newPostElement}></textarea>
         </div>
         <div>
-          <button>Add Post</button>
+          <button onClick={ addPost }>Add Post</button>
         </div>
       </div>
       <div className={classes.posts}>
